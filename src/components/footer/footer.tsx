9df90971daf196d6b2a102a207215afae18bf905
abdurazzzoq <salemialekum34@gmail.com>
2023-05-01 23:17:42 +0500

    header & footer component

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,31 @@
+import { Box, Button, ButtonGroup, Typography } from "@mui/material";
+import { format } from "date-fns";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+
+const Footer = () => {
+  return (
+    <Box
+      padding={"20px"}
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "primary.main",
+        color: "white",
+      }}
+    >
+      <Typography>{format(new Date(), "yyyy")}. All rights reserved</Typography>
+      <Box
+        sx={{display: "flex",gap:"10px",alignItems: "center"}}
+      >
+        <TelegramIcon sx={{cursor:'pointer'}}/>
+        <InstagramIcon sx={{cursor:'pointer'}}/>
+        <FacebookIcon sx={{cursor:'pointer'}}/>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,40 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import createEmotionCache from "@/helpers/create-emotion-cache";
+import theme from "@/helpers/theme";
+import "@/styles/globals.css";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider } from "@mui/material/styles";
+import type { AppProps } from "next/app";
+import CssBaseline, {} from '@mui/material/CssBaseline'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Roboto } from "next/font/google";
+import Head from "next/head";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+
+<CacheProvider value={emotionCache}>
+<Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+    </ThemeProvider>
+    </CacheProvider>
+  );
 }
